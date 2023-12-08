7f135017f64bcb6bfead3e2e841aa00dc57f6ebf
Evann <engt2003@gmail.com>
2023-12-08 11:02:54 +0100

    optimized bound function

--- UBER-IF/src/main/java/com/malveillance/uberif/model/algo/TSP1.java
+++ UBER-IF/src/main/java/com/malveillance/uberif/model/algo/TSP1.java
@@ -9,7 +9,34 @@ import java.util.Iterator;
 public class TSP1 extends TemplateTSP {
 	@Override
 	protected int bound(Delivery currentVertex, Collection<Delivery> unvisited) {
-		return 0;
+		if (unvisited.isEmpty()) {
+			return 0;
+		}
+
+		double bound = CityMap.INFINITE_LENGTH;
+		for (Delivery v : unvisited) {
+			double distance = g.getDistance(currentVertex.getIntersection(), v.getIntersection());
+			if (distance < bound) {
+				bound = distance;
+			}
+		}
+
+		for (Delivery v1 : unvisited) {
+			double nearestNeighborDistance = CityMap.INFINITE_LENGTH;
+			for (Delivery v2 : unvisited) {
+				if (!v1.equals(v2)) {
+					double distance = g.getDistance(v1.getIntersection(), v2.getIntersection());
+					if (distance < nearestNeighborDistance) {
+						nearestNeighborDistance = distance;
+					}
+				}
+			}
+			if (nearestNeighborDistance < CityMap.INFINITE_LENGTH) {
+				bound += nearestNeighborDistance;
+			}
+		}
+
+		return (int)Math.ceil(bound);
 	}
 
 	@Override
