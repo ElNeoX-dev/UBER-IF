bc0e8c680aa812f06e1bb5c2334264093ae2b940
Evann <engt2003@gmail.com>
2023-12-22 22:06:21 +0100

    pdf fix

--- UBER-IF/src/main/java/com/malveillance/uberif/formatters/PDFRoadMap.java
+++ UBER-IF/src/main/java/com/malveillance/uberif/formatters/PDFRoadMap.java
@@ -139,6 +139,7 @@ public class PDFRoadMap {
                                 contentStream = new PDPageContentStream(document, page);
                                 contentStream.setFont(PDType1Font.HELVETICA, 11);
                                 contentStream.beginText();
+                                contentStream.newLineAtOffset(100, 700);
                             }
                         }
                     }
@@ -157,6 +158,7 @@ public class PDFRoadMap {
                     contentStream = new PDPageContentStream(document, page);
                     contentStream.setFont(PDType1Font.HELVETICA, 11);
                     contentStream.beginText();
+                    contentStream.newLineAtOffset(100, 700);
                 }
 
 
